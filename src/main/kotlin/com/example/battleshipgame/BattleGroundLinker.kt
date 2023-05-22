93c234e1a2220d0bde5d28ccf7ee48f3c6352462
CARTER SPEERSCHNEIDER (1071831) <1071831@lcps.org>
2023-05-22 12:39:57 -0400

    bla

--- src/main/kotlin/com/example/battleshipgame/BattleGroundLinker.kt
+++ src/main/kotlin/com/example/battleshipgame/BattleGroundLinker.kt
@@ -4,11 +4,16 @@ import carte.toolfx.core.Controller
 import carte.toolfx.core.runFxmlElement
 import com.example.battleshipgame.controllers.BattleTileController
 import com.example.battleshipgame.model.BattleGround
+import com.example.battleshipgame.model.BattleTile
 import javafx.scene.Node
 import javafx.scene.control.ButtonType
 import javafx.scene.control.Dialog
 import javafx.scene.layout.GridPane
 import javafx.scene.paint.Color
+import javafx.scene.paint.ImagePattern
+import javafx.scene.shape.LineTo
+import javafx.scene.shape.MoveTo
+import javafx.scene.shape.Path
 import javafx.scene.shape.Rectangle
 import java.util.function.Consumer
 
@@ -17,11 +22,17 @@ class BattleGroundLinker(
     private val battleGroundPane: GridPane
 ) {
     fun reset() {
+        battleGroundPane.children.clear();
         battleGround.reset()
         battleGround.placeShip();
     }
 
-    private fun applyToRectangles(eachRectangle: (r: Int, c: Int) -> Unit) {
+    private fun applyAndAddRectangles() {
+
+
+    }
+
+    private fun applyToBattleTiles(eachRectangle: (r: Int, c: Int) -> Unit) {
 
 
         for (r in battleGround.board.indices) {
@@ -31,6 +42,13 @@ class BattleGroundLinker(
         }
     }
 
+    fun showShip() {
+        battleGroundPane.children.clear();
+
+
+
+    }
+
     fun link() {
 
         battleGround.placeShip()
@@ -38,27 +56,52 @@ class BattleGroundLinker(
 
 
 
-        applyToRectangles { r, c ->
+        applyToBattleTiles { r, c ->
 
 
             battleGroundPane.add(runFxmlElement<BattleTileController>(context) {
                 battleTileRectangle.setOnMouseClicked {
                     val battleTile = battleGround.board[r][c] ?: error("REEEEEEEEEEEEEEEEEEEE")
-                    if (battleTile.isShip) {
+                    if (battleTile.isShip && !battleTile.isHit && battleGround.hits > 0) {
                         battleTileRectangle.fill = Color.INDIANRED;
-
-
                         battleGround.hits--;
+                        battleGround.board[r][c] = BattleTile(isHit = true, isShip = true)
                         val hitDialog = Dialog<String>();
                         hitDialog.dialogPane.buttonTypes.add(ButtonType.OK)
                         hitDialog.contentText = "you hit the ship with ${battleGround.hits} hits left!"
                         hitDialog.showAndWait();
-                    } else {
+                    } else if (!battleTile.isHit && battleGround.hits > 0) {
                         val noHitDialog = Dialog<String>();
                         battleGround.hits--;
+
+                        battleGroundPane.add(
+                            Path(
+                                MoveTo(0.0, 0.0),
+                                LineTo(50.0, 50.0),
+                                MoveTo(50.0, 0.0),
+                                LineTo(0.0, 50.0)
+                            ).apply {
+                                translateX += 12;
+                            }, c, r
+                        );
+                        battleGround.board[r][c] = BattleTile(isHit = true, isShip = false)
                         noHitDialog.contentText = "did not hit a ship. you have ${battleGround.hits} hits left"
                         noHitDialog.dialogPane.buttonTypes.add(ButtonType.OK)
                         noHitDialog.showAndWait()
+
+                    } else if (battleGround.hits == 0) {
+                        val noHitsLeftDialog = Dialog<String>();
+                        noHitsLeftDialog.contentText = "you have no hits left!"
+                        applyToRec { r, c ->
+                            val battleTile = battleGround.board[r][c] ?: error("REEEEEEEEEEEEEEEEEEEE")
+
+
+                            if (battleTile.isShip) {
+
+
+                            }
+
+                        }
 
                     }
 
